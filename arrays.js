042a592ce2d114b60a93d17229b269d46f0e4e4e
Joshua M Loran <joshua.loran@gmail.com>
2018-12-05 16:17:08 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+function addElementToBeginningOfArray(a, b) {
+  return [b, ...a]
+}
+function destructivelyAddElementToBeginningOfArray(a,b) {
+  a.unshift(b)
+  return a
+}
+function addElementToEndOfArray(a,b) {
+  return [...a,b]
+}
+function destructivelyAddElementToEndOfArray(a,b) {
+  a.push(b)
+  return a
+}
+function accessElementInArray(a, index) {
+  return a[index]
+}
+function destructivelyAddElementToEndOfArray (a) {
+  
+
+
+
+  
+
+  
+
+
+  
+
+  
